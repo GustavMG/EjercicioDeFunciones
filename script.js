0a0fd0b72ff48e50672931d803f5b6387185f0c9
GustavMG <magg950506@gmail.com>
2023-05-23 17:47:09 -0600

    Reducción de lineas en función formulaGeneral()

--- script.js
+++ script.js
@@ -32,9 +32,16 @@ let b = prompt("Ingresa el valor de b");
 let c = prompt("Ingresa el valor de c");
 
 formulaGeneral(a, b, c);
+// formulaGeneral(6, -19, 7);   // 2.741036999120817 y 0.4256296675458497
+// formulaGeneral(1, -4, 3);    // 3 y 1 
+// formulaGeneral(1, 4, 3);     // -1 y -3
+// formulaGeneral(4, 8, -12);   // 1 y -3
+// formulaGeneral(4, 12, 9);    // -3/2
 
 function formulaGeneral(a, b, c){
 
+    /* 
+    // Version larga
     let resultadoX1;
     let resultadoX2;
     let tempA;
@@ -47,24 +54,16 @@ function formulaGeneral(a, b, c){
     tempB = b * b;
     tempBC = tempB - tempAC
     tempCuadrado = Math.sqrt(tempBC);
-    tempA = 2 * a;
-    
-    resultadoX1 = (-b + tempCuadrado) / tempA;
-    resultadoX2 = (-b - tempCuadrado) / tempA;
+    tempA = 2 * a;*/
 
-    /*console.log(tempAC);
-    console.log(tempB);
-    console.log(tempBC);
-    console.log(tempA);
-    console.log(tempCuadrado);*/
+    // Version Corta
+    interiorRaiz = ((b * b) - (4 * a * c));
+    raiz = Math.sqrt(interiorRaiz);
+    
+    resultadoX1 = (-b + raiz) / (2 * a);
+    resultadoX2 = (-b - raiz) / (2 * a);
 
     console.log("Dada la expresión " + a + "x^2 + (" + b + ")x + " + c + ",");
     console.log("La solución de x1 es: " + resultadoX1 + " y la solución de x2 es: " + resultadoX2);
 
 }
-
-// formulaGeneral(6, -19, 7);   // 2.741036999120817 y 0.4256296675458497
-// formulaGeneral(1, -4, 3);    // 3 y 1 
-// formulaGeneral(1, 4, 3);     // -1 y -3
-// formulaGeneral(4, 8, -12);   // 1 y -3
-// formulaGeneral(4, 12, 9);    // -3/2
